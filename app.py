ffa99da2d657ac645c31a720b0da1192e1adc520
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 19:04:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,21 @@ from datetime import date
 # Load data
 df = pl.read_csv("tjstuff_plus_pitch_data_2024.csv").fill_nan(None)
 df = df.drop_nulls(subset=['pitch_grade','tj_stuff_plus'])
+df = df.sort(['pitcher_name','tj_stuff_plus'], ascending=[False,True])
 
-
-df = df.with_columns([
-    pl.col('tj_stuff_plus').cast(pl.Int64).alias('tj_stuff_plus'),
-    pl.col('pitches').cast(pl.Int64).alias('pitches'),
-    pl.col('pitcher_id').cast(pl.Int64).alias('pitcher_id'),
-    pl.col('pitch_grade').cast(pl.Int64).alias('pitch_grade')
-])
+# df = df.with_columns([
+#     pl.col('tj_stuff_plus').cast(pl.Int64).alias('tj_stuff_plus'),
+#     pl.col('pitches').cast(pl.Int64).alias('pitches'),
+#     pl.col('pitcher_id').cast(pl.Int64).alias('pitcher_id'),
+#     pl.col('pitch_grade').cast(pl.Int64).alias('pitch_grade')
+# ])
 column_config_dict = {
     'pitcher_id': 'Pitcher ID',
     'pitcher_name': 'Pitcher Name',
     'pitch_type': 'Pitch Type',
     'pitches': 'Pitches',
-    'tj_stuff_plus': 'tjStuff+',
-    'pitch_grade': 'Grade'
+    'tj_stuff_plus': st.column_config.NumberColumn("tjStuff+", format="%.0f%"),
+    'pitch_grade': st.column_config.NumberColumn("Pitch Grade", format="%.0f%")
 }
 
 

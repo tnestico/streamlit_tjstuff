fd5f25e86d230e172f4a946d1932cd8c56710574
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 19:16:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ from datetime import date
 import pandas as pd
 import matplotlib
 
+# Define a custom colormap for styling
 cmap_sum = matplotlib.colors.LinearSegmentedColormap.from_list("", ['#648FFF','#FFFFFF','#FFB000',])
 
-
-# Load data
+# Load data from CSV file and preprocess it
 df = pl.read_csv("tjstuff_plus_pitch_data_2024.csv").fill_nan(None)
 df = df.filter(df['pitches']>=10).drop_nulls(subset=['pitch_grade','tj_stuff_plus'])
 df = df.sort(['pitcher_name','pitch_type'], descending=[False,False])
 
+# Cast columns to appropriate data types
 df = df.with_columns([
     pl.col('tj_stuff_plus').cast(pl.Int64).alias('tj_stuff_plus'),
     pl.col('pitches').cast(pl.Int64).alias('pitches'),
@@ -22,6 +23,7 @@ df = df.with_columns([
     pl.col('pitch_grade').cast(pl.Int64).alias('pitch_grade')
 ])
 
+# Define column configuration for Streamlit
 column_config_dict = {
     'pitcher_id': 'Pitcher ID',
     'pitcher_name': 'Pitcher Name',
@@ -31,12 +33,14 @@ column_config_dict = {
     'pitch_grade': st.column_config.NumberColumn("Pitch Grade", format="%.0f")
 }
 
+# Convert Polars DataFrame to Pandas DataFrame and apply styling
 styled_df = df[['pitcher_id', 'pitcher_name', 'pitch_type', 'pitches', 'tj_stuff_plus', 'pitch_grade']].to_pandas().style
 
-styled_df = styled_df.background_gradient(subset=['tj_stuff_plus'], cmap=cmap_sum,vmin=85,vmax=115)
+# Apply background gradient styling to specific columns
+styled_df = styled_df.background_gradient(subset=['tj_stuff_plus'], cmap=cmap_sum,vmin=80,vmax=120)
 styled_df = styled_df.background_gradient(subset=['pitch_grade'], cmap=cmap_sum,vmin=20,vmax=80)
 
-
+# Display the styled DataFrame in Streamlit
 st.dataframe(styled_df,
                 hide_index=True,
                 column_config=column_config_dict,

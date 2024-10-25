2ad2fcee65417944706d747fd138e009ac820e87
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 18:55:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ from datetime import date
 df = pl.read_csv("tjstuff_plus_pitch_data_2024.csv")
 
 
+df['tj_stuff_plus'] = df['tj_stuff_plus'].cast(pl.Int64)
+df['pitches'] = df['pitches'].cast(pl.Int64)
+df['pitcher_id'] = df['pitcher_id'].cast(pl.Int64)
+df['pitch_grade'] = df['pitch_grade'].cast(pl.Int64)
+
+
 column_config_dict = {
     'pitcher_id': 'Pitcher ID',
     'pitcher_name': 'Pitcher Name',
@@ -17,6 +23,9 @@ column_config_dict = {
     'tj_stuff_plus': 'tjStuff+',
     'pitch_grade': 'Grade'
 }
+
+
+
 
 st.dataframe(df[['pitcher_id', 'pitcher_name', 'pitch_type', 'pitches', 'tj_stuff_plus', 'pitch_grade']],
                 hide_index=True,

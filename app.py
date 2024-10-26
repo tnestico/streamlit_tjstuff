6c3ea030d06b5e6b7b00610c025a4f30ca481ad8
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 21:31:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ unique_pitch_types = df['pitch_type'].unique().to_list()
 if 'selected_pitch_types' not in st.session_state:
     st.session_state.selected_pitch_types = unique_pitch_types
 
-selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types, default='')
+selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types, default='All')
 
 # Filter the DataFrame based on selected pitch types
 if selected_pitch_types:

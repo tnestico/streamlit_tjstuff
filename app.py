7381283c25a78f455fe7ebaa0afafc98407b2e5f
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 21:33:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,14 @@ unique_pitch_types = df['pitch_type'].unique().to_list()
 if 'selected_pitch_types' not in st.session_state:
     st.session_state.selected_pitch_types = unique_pitch_types
 
-selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types)
-
 # Filter the DataFrame based on selected pitch types
 if selected_pitch_types:
     df = df.filter(pl.col('pitch_type').is_in(selected_pitch_types))
     st.session_state.selected_pitch_types = selected_pitch_types
+
+selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types)
+
+
 
 # Convert Polars DataFrame to Pandas DataFrame and apply styling
 styled_df = df[['pitcher_id', 'pitcher_name', 'pitch_type', 'pitches', 'tj_stuff_plus', 'pitch_grade']].to_pandas().style

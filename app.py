4acad3af32c8647c3b3c569e506b2aff9d16899d
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 21:33:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,11 @@ column_config_dict = {
 # Get unique pitch types for multiselection
 unique_pitch_types = df['pitch_type'].unique().to_list()
 
+
+
+
+selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types)
+
 # Create a multiselect widget for pitch types
 if 'selected_pitch_types' not in st.session_state:
     st.session_state.selected_pitch_types = unique_pitch_types
@@ -49,10 +54,6 @@ if 'selected_pitch_types' not in st.session_state:
 if selected_pitch_types:
     df = df.filter(pl.col('pitch_type').is_in(selected_pitch_types))
     st.session_state.selected_pitch_types = selected_pitch_types
-
-selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types)
-
-
 
 # Convert Polars DataFrame to Pandas DataFrame and apply styling
 styled_df = df[['pitcher_id', 'pitcher_name', 'pitch_type', 'pitches', 'tj_stuff_plus', 'pitch_grade']].to_pandas().style

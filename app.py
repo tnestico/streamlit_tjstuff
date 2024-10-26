0f8cd2c03dd10a1bfaa032d4b1660e082b8993de
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 21:59:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -89,10 +89,11 @@ unique_pitch_types = [dict_pitch[x] if x in dict_pitch else x for x in unique_pi
 selected_pitch_types = st.selectbox('Select Pitch Types', unique_pitch_types)
 
 # Filter the DataFrame based on selected pitch types
-if selected_pitch_types != '':
-    df = df.filter(pl.col('pitch_type')==dict_pitch_desc_type(selected_pitch_types)).sort('tj_stuff_plus', descending=True)
-# if selected_pitch_types == 'All':
-#     df = df.filter(pl.col('pitch_type')=='All').sort('tj_stuff_plus', descending=True)
+if selected_pitch_types == 'All':
+    df = df.filter(pl.col('pitch_type')=='All').sort('tj_stuff_plus', descending=True)
+elif selected_pitch_types != '':
+    df = df.filter(pl.col('pitch_type')==dict_pitch_desc_type[selected_pitch_types]).sort('tj_stuff_plus', descending=True)
+
 
 # Convert Polars DataFrame to Pandas DataFrame and apply styling
 styled_df = df[['pitcher_id', 'pitcher_name', 'pitch_type', 'pitches', 'tj_stuff_plus', 'pitch_grade']].to_pandas().style

ac56d398d8a275bbb9cbb86856c84cca6b8b99a0
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 22:36:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ pitcher_id = pitcher_name_id_id[pitcher_id_name]
 # Define pitcher ID and position
 position = pitcher_id_position[pitcher_id]
 st.text(pitcher_id)
-pitcher_name = pitcher_id_name[pitcher_id]
+pitcher_name = pitcher_id_name[inst(pitcher_id)]
 
 import tjstuff_plot
 

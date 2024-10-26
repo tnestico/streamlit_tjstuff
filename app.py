01985565b5f794b629ef43f67d785f01820a821f
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 21:41:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -43,10 +43,8 @@ unique_pitch_types = df['pitch_type'].unique().to_list()
 
 # Initialize session state for selected pitch types
 if 'selected_pitch_types' not in st.session_state:
-    st.session_state.selected_pitch_types = unique_pitch_types
-
-# Multiselect widget for pitch types
-selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types, default=st.session_state.selected_pitch_types)
+    # Multiselect widget for pitch types
+    selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types, default=st.session_state.selected_pitch_types)
 
 # Update session state
 st.session_state.selected_pitch_types = selected_pitch_types

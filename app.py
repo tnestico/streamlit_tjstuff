e45756783fea2ef8abffe270ccb786e01f00bbd4
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 22:15:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -110,10 +110,12 @@ st.dataframe(styled_df,
                 width=1500)
 
 
+
 pitcher_id_name = dict(zip(df_plot['pitcher_id'],df_plot['pitcher_name']))
 pitcher_id_name_id = dict(zip(df_plot['pitcher_id'],df_plot['pitcher_name']+ ' - '+df_plot['pitcher_id']))
-pitcher_name_id_id = dict(zip(df_plot['pitcher_id'],df_plot['pitcher_name']+ ' - '+df_plot['pitcher_id']))
+pitcher_name_id_id = dict(zip(df_plot['pitcher_name']+ ' - '+df_plot['pitcher_id'],df_plot['pitcher_id']))
 pitcher_id_position = dict(zip(df_plot['pitcher_id'],df_plot.drop_nulls(subset=['position'])['position']))
+
 
 
 # Create a multiselect widget for pitch types

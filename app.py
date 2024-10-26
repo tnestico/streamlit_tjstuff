831354dcc82f716e2bd13ad92811cd40e0bed2fe
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 21:37:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ unique_pitch_types = df['pitch_type'].unique().to_list()
 
 selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types)
 
-while 'selected_pitch_types' in st.session_state:
+if 'selected_pitch_types' in st.session_state:
     df = df.filter(pl.col('pitch_type').is_in(selected_pitch_types))
     #st.session_state.selected_pitch_types = selected_pitch_types
     

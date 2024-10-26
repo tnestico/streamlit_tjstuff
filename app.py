6c87f954dfae335233dc7712c014382436bd427b
tnestico <123529071+tnestico@users.noreply.github.com>
2024-10-25 21:42:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,12 +23,7 @@ df = df.with_columns([
     pl.col('pitch_grade').cast(pl.Int64).alias('pitch_grade')
 ])
 
-
-# Drop rows with NaN values and sort the DataFrame
-df = df.drop_nulls(subset=['pitch_grade', 'tj_stuff_plus'])
-df = df.sort(['pitcher_name', 'pitch_type'], descending=[False, False])
-
-# Define column configuration
+# Define column configuration for Streamlit
 column_config_dict = {
     'pitcher_id': 'Pitcher ID',
     'pitcher_name': 'Pitcher Name',
@@ -38,27 +33,27 @@ column_config_dict = {
     'pitch_grade': st.column_config.NumberColumn("Pitch Grade", format="%.0f")
 }
 
+
 # Get unique pitch types for multiselection
 unique_pitch_types = df['pitch_type'].unique().to_list()
 
-# Initialize session state for selected pitch types
-if 'selected_pitch_types' in st.session_state:
-    # Multiselect widget for pitch types
-    selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types, default=st.session_state.selected_pitch_types)
+# Create a multiselect widget for pitch types
+selected_pitch_types = st.multiselect('Select Pitch Types', unique_pitch_types)
 
-    # Update session state
-    st.session_state.selected_pitch_types = selected_pitch_types
+# Filter the DataFrame based on selected pitch types
+if selected_pitch_types:
+    df = df.filter(pl.col('pitch_type').is_in(selected_pitch_types))
 
-    # Filter DataFrame based on selected pitch types
-    if selected_pitch_types:
-        df = df.filter(pl.col('pitch_type').is_in(selected_pitch_types))
 
 # Convert Polars DataFrame to Pandas DataFrame and apply styling
 styled_df = df[['pitcher_id', 'pitcher_name', 'pitch_type', 'pitches', 'tj_stuff_plus', 'pitch_grade']].to_pandas().style
 
 # Apply background gradient styling to specific columns
-styled_df = styled_df.background_gradient(subset=['tj_stuff_plus'], cmap='viridis', vmin=80, vmax=120)
-styled_df = styled_df.background_gradient(subset=['pitch_grade'], cmap='viridis', vmin=20, vmax=80)
+styled_df = styled_df.background_gradient(subset=['tj_stuff_plus'], cmap=cmap_sum,vmin=80,vmax=120)
+styled_df = styled_df.background_gradient(subset=['pitch_grade'], cmap=cmap_sum,vmin=20,vmax=80)
 
 # Display the styled DataFrame in Streamlit
-st.dataframe(styled_df, hide_index=True, column_config=column_config_dict, width=1500)
+st.dataframe(styled_df,
+                hide_index=True,
+                column_config=column_config_dict,
+                width=1500)
